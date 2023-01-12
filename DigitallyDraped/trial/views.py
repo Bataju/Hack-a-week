eff4ddc516ade01c990ff29ab0317f6ebfd811a4
Bataju <kashishbataju999@gmail.com>
2023-01-12 20:07:09 +0545

    backend halka fulka

--- DigitallyDraped/trial/views.py
+++ DigitallyDraped/trial/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here
+import openai
+import json
+
+openai.api_key = "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"
 
 def index(request):
-    #return HttpResponse("THIS IS THE HOME PAGE")
-    return render(request, "trial\index.html")
+    if request.method == 'POST':
+        description = request.POST['description']
+
+        # Use the text input as the prompt for the API
+        response = openai.Image.create(
+            prompt=description,
+            n=1,
+        )
+
+        j_response=json.dumps(response)
+        parsed_response = json.loads(j_response)
+        data = parsed_response["data"][0]
+        image_url=data['url']
+        
+        return render(request, 'trial\image.html', {'image_url': image_url})
+    else:
+        return render(request, 'trial\index.html')
