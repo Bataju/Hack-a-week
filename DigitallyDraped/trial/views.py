676764ca3bbcd07ee7c6d58727a37ba64c6c2d28
Bataju <kashishbataju999@gmail.com>
2023-01-12 20:55:07 +0545

    images changed

--- DigitallyDraped/trial/views.py
+++ DigitallyDraped/trial/views.py
@@ -12,14 +12,14 @@ def index(request):
         # Use the text input as the prompt for the API
         response = openai.Image.create(
             prompt=description,
-            n=1,
+            n=2,
         )
 
         j_response=json.dumps(response)
         parsed_response = json.loads(j_response)
         data = parsed_response["data"][0]
         image_url=data['url']
-        
+
         return render(request, 'trial\image.html', {'image_url': image_url})
     else:
         return render(request, 'trial\index.html')
